82c73aa3285d5ca4fb6e1e3b7faa5075ce93469e
Son, Eunhyeok <dmsgur12772@gmail.com>
2025-05-21 09:25:25 +0900

    three

--- src/g202014076/Main.java
+++ src/g202014076/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 	  public static void main(String[] args) {
 	    System.out.println("손은혁");
+		World.run();
 	  }
 	}
---
+++ src/g202014076/World.java
@@ -0,0 +1,8 @@
+package g202014076;
+
+public class World {
+
+  public static void run() {
+    System.out.println("202014076");
+  }
+}
